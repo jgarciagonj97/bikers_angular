58aaa1d4aa50a97c2f14373c9346bb1c2e7d12ea
ivangm13 <ivangarcia346@gmail.com>
2020-06-03 17:06:23 +0200

    inicio seccion soporte

--- ProyectoFinal-Angular/src/app/app.module.ts
+++ ProyectoFinal-Angular/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- ProyectoFinal-Angular/src/app/soporte/soporte.component.ts
+++ ProyectoFinal-Angular/src/app/soporte/soporte.component.ts
@@ -12,4 +12,6 @@ export class SoporteComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  
 }
+ 
