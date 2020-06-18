55b6aff905cf091f34565377e7fcc5621dce8c7e
jgarciagonj97 <j.garciagonj@gmail.com>
2020-06-18 20:09:48 +0200

    Intento de login y pequeña modificación del guard. Mirar si se puede validar que el token sea correcto.

--- ProyectoFinal-Angular/src/app/componentes/formulario/formulario.component.ts
+++ ProyectoFinal-Angular/src/app/componentes/formulario/formulario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpHeaders, HttpRequest, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-formulario',
@@ -7,14 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
+  formulario: FormGroup;
   fechaActual: any;
-  constructor() {
-    this.fechaActual = new Date(Date.now())
-    .toLocaleDateString()
-    .replace(/\//g, '-');
-   }
+  files;
+
+  constructor(private http: HttpClient) {
+    this.formulario = new FormGroup({
+      titulo: new FormControl(""),
+      autor: new FormControl(""),
+      texto: new FormControl(""),
+      file: new FormControl("")
+    });
+    // this.fechaActual = new Date(Date.now())
+    //   .toLocaleDateString()
+    //   .replace(/\//g, '-');
+  }
 
   ngOnInit(): void {
+  }
+
+  onFileChange($event) {
+    this.files = $event.target.files;
+  }
+
+  onSubmit() {
+    let fd = new FormData();
+    fd.append("imagen", this.files[0], "nuevaImagen.png");
+    fd.append("titulo", this.formulario.controls.titulo.value);
+    let header = new HttpHeaders();
+    header.append('Content-Type', 'multipart/form-data');
+    const req = new HttpRequest("POST", "http://localhost:3000", fd, {
+      headers: header
+    });
+    this.http
+      .request(req)
+      .toPromise()
+      .then(result => {
+        console.log(result);
+      });
   }
 
 }
--- ProyectoFinal-Angular/src/app/servicios/users.service.ts
+++ ProyectoFinal-Angular/src/app/servicios/users.service.ts
@@ -7,14 +7,18 @@ import { PostService } from './post.service';
 })
 export class UsersService {
   baseUrl: string;
-  
+
   constructor(private httpClient: HttpClient, private postService: PostService) {
     this.baseUrl = 'https://bikers1.herokuapp.com';
   }
 
+  // async compruebaToken() {
+  //   const tokencillo = await this.httpClient.post(`${this.baseUrl}/login`);
+  //   return tokencillo;
+  // }
 
   async actualizarPerfil(formValues): Promise<any> {
-    const id= await this.postService.obtenerId(formValues.email);
+    const id = await this.postService.obtenerId(formValues.email);
     return this.httpClient
       .put(`${this.baseUrl}/api/users/${id[0].id}`, formValues)
       .toPromise();
