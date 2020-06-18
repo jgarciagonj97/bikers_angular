428e9d282df4a6cc6018627590fc920106dde7e8
ivangm13 <ivangarcia346@gmail.com>
2020-06-18 10:06:54 +0200

    Funcionamiento botón crear post correcto

--- ProyectoFinal-Angular/src/app/app.component.ts
+++ ProyectoFinal-Angular/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { PostService } from './servicios/post.service';
+import { Location } from '@angular/common';
+
 
 
 @Component({
@@ -10,21 +12,24 @@ import { PostService } from './servicios/post.service';
 })
 export class AppComponent {
 
-  href: string;
-  rol:string;
   
-  constructor(public router: Router,private postService:PostService) {
-    //this.rol = null;
-    
-   }
+  rol: string;
 
-   ngOnInit():void {
-     this.rol = localStorage.getItem('rol');
-    console.log(this.rol);
+  constructor(public router: Router, private postService: PostService, private location: Location) {
+    router.events.subscribe(() => {
+      if (location.path() != "") this.rol = localStorage.getItem('rol');
+    });
   }
 
-  
-
-
+  ngOnInit(): void {
+    
+  }
 }
+
+
+
+
+
+
+
 
