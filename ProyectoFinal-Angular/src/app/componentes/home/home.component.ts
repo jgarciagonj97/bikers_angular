19c3b5395d0a19e68cfe7cf6a78046dd371ff135
jonhlop <jonathan.lopher@hotmail.com>
2020-06-22 17:32:04 +0200

    validators

--- ProyectoFinal-Angular/src/app/componentes/home/home.component.ts
+++ ProyectoFinal-Angular/src/app/componentes/home/home.component.ts
@@ -1,33 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from 'src/app/servicios/post.service';
 import { PostUsuario } from 'src/app/models/postUsuario.model';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  formNewpost: FormGroup
-  arrNovedades:PostUsuario[];
-  constructor(private postService: PostService) { 
-    this.arrNovedades = new Array()
+  formNewpost: FormGroup;
+  arrNovedades: PostUsuario[];
+  constructor(private postService: PostService) {
+    this.arrNovedades = new Array();
     this.formNewpost = new FormGroup({
-      titulo: new FormControl(''),
-      descripcion: new FormControl(''),
-      id: new FormControl(parseInt(localStorage.getItem('id')))
-    })
+      titulo: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      descripcion: new FormControl('', [Validators.required]),
+      id: new FormControl(parseInt(localStorage.getItem('id'))),
+    });
   }
 
-   async ngOnInit() {
-  this.arrNovedades = await this.postService.cargarNovedades();
-  console.log(this.arrNovedades)
+  async ngOnInit() {
+    this.arrNovedades = await this.postService.cargarNovedades();
+    console.log(this.arrNovedades);
   }
 
-  async onSubmit(){
+  async onSubmit() {
     /* crear post*/
-    await this.postService.crearPost(this.formNewpost.value)
-
+    await this.postService.crearPost(this.formNewpost.value);
   }
 }
