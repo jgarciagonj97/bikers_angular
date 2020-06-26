7164819ed1f379236e35f071ab040fb6f63e88e5
ivangm13 <ivangarcia346@gmail.com>
2020-06-26 16:43:09 +0200

    cambios firebase

--- ProyectoFinal-Angular/src/environments/environment.prod.ts
+++ ProyectoFinal-Angular/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAOMuFCiVp_zzBvMJz-ivmcUFlF8FpcBmA",
-    authDomain: "bikersapp-44446.firebaseapp.com",
-    databaseURL: "https://bikersapp-44446.firebaseio.com",
-    projectId: "bikersapp-44446",
-    storageBucket: "bikersapp-44446.appspot.com",
-    messagingSenderId: "677457742908",
-    appId: "1:677457742908:web:b5c334575faaffc2e40739",
-    measurementId: "G-G05LFQ58MZ"
+    apiKey: "AIzaSyBq8FhnEoG1kKHnu85Izd6w0JsjpJJVlrk",
+    authDomain: "bikers-24e17.firebaseapp.com",
+    databaseURL: "https://bikers-24e17.firebaseio.com",
+    projectId: "bikers-24e17",
+    storageBucket: "bikers-24e17.appspot.com",
+    messagingSenderId: "585057434491",
+    appId: "1:585057434491:web:b3f027aefa364285b01bde",
+    measurementId: "G-XYWJQ1VDR9"
   }
 };
